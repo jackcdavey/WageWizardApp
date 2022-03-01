31a13c970b91549f15d00b0fa7157a3f2c72e04b
Jack Davey <jdavey@scu.edu>
2022-03-01 14:42:51 -0800

    Added placeholder components for remaining views, updated readme styling

---
+++ src/components/DetailedLogView.tsx
@@ -0,0 +1,9 @@
+// gonna be useful: https://reactnavigation.org/docs/hiding-tabbar-in-screens
+
+import { View } from "react-native";
+
+export default function DetailedLogView(){
+    return(
+    <View></View>
+    )
+}
