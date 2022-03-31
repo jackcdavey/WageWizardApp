3d5a29fa43effd2bd05415e691977dd1510a7a79
KyleMondina <kylemondina@gmail.com>
2022-03-31 14:01:31 -0700

    added polygon fences

--- src/components/elements/LocationTrackingMap.js
+++ src/components/elements/LocationTrackingMap.js
@@ -1,6 +1,8 @@
 import * as React from 'react';
 import MapView from 'react-native-maps';
 import { Marker } from 'react-native-maps';
+import { Polygon } from 'react-native-maps';
+
 
 import { SafeAreaView, ScrollView, StatusBar, Dimensions, StyleSheet, Text, useColorScheme, View, TouchableOpacity, Alert, } from 'react-native';
 import { useState, useEffect } from 'react';
@@ -17,6 +19,46 @@ let foregroundSubscription = null
 
 const LATITUDE_DELTA = 0.000922;
 const LONGITUDE_DELTA = 0.000421;
+
+const heafey = [
+    {latitude:37.349328,longitude:-121.939938},
+    {latitude:37.349439,longitude:-121.939642},
+    {latitude:37.348811,longitude:-121.939293},
+    {latitude:37.348684,longitude:-121.939840}
+    
+]
+
+const scdi = [
+    {latitude:37.349400,longitude:-121.939233},
+    {latitude:37.349774,longitude:-121.938308},
+    {latitude:37.348773,longitude:-121.937745},
+    {latitude:37.348507,longitude:-121.938735}
+    
+]
+
+const kenna = [
+    {latitude:37.348378,longitude:-121.940388},
+    {latitude:37.348631,longitude:-121.939641},
+    {latitude:37.348430,longitude:-121.939603},
+    {latitude:37.348226,longitude:-121.940280}
+    
+]
+
+const benson = [
+    {latitude:37.347885,longitude:-121.939971},
+    {latitude:37.348227,longitude:-121.938994},
+    {latitude:37.347582,longitude:-121.938632},
+    {latitude:37.347225,longitude:-121.939679}
+    
+]
+
+const nobili = [
+    {latitude:37.349194,longitude:-121.942696},
+    {latitude:37.349262,longitude:-121.942437},
+    {latitude:37.348686,longitude:-121.942109},
+    {latitude:37.348592,longitude:-121.942361}
+    
+]
 
 
 export default function LocationMap() {
@@ -141,6 +183,11 @@ const stopForegroundUpdate = () => {
             <Marker
                 coordinate = {{latitude:region.latitude , longitude:region.longitude}}
             />
+            <Polygon coordinates = {heafey} strokeWidth = {3} fillColor = {'rgba(245, 40, 145, 0.35)'}/>
+            <Polygon coordinates = {scdi} strokeWidth = {3} fillColor = {'rgba(245, 40, 145, 0.35)'}/>
+            <Polygon coordinates = {kenna} strokeWidth = {3} fillColor = {'rgba(245, 40, 145, 0.35)'}/>
+            <Polygon coordinates = {benson} strokeWidth = {3} fillColor = {'rgba(245, 40, 145, 0.35)'}/>
+            <Polygon coordinates = {nobili} strokeWidth = {3} fillColor = {'rgba(245, 40, 145, 0.35)'}/>
 
         </MapView>
         <TouchableOpacity style = {{backgroundColor:locationButtonColor}} onPress={handleLocationButton}>
