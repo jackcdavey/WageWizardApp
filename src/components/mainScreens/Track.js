d43a1fc55d37028f4e7d8a7f31b8c31ea7a3e0c6
jackcdavey <jdavey@scu.edu>
2022-04-09 16:40:41 -0700

    Tracking view style update

--- src/components/mainScreens/Track.js
+++ src/components/mainScreens/Track.js
@@ -11,7 +11,7 @@ import styles from '../../styles/stylesheet';
 //import JobLocationSetup from '../setupScreens/AddJobLocation';
 
 import realm from '../../userData/realm';
-import {ObjectId} from 'bson'
+import { ObjectId } from 'bson'
 
 //import realm from '../../userData/realm';
 
@@ -26,7 +26,7 @@ const mapStateToProps = (state, props) => {
 }
 const mapDispatchToProps = (dispatch, props) => {
   return {
-    setJobId: (jobId)=>{dispatch(setJobId(jobId))}
+    setJobId: (jobId) => { dispatch(setJobId(jobId)) }
   }
 }
 
@@ -37,7 +37,7 @@ const workLogDebugInfo = true;
 
 const _Tracking = (props) => {
 
-  const {setJobId,jobId,isTracking} = props
+  const { setJobId, jobId, isTracking } = props
   const [open, setOpen] = useState(false);
   //Something messy going on with setValue being passed to the dropdown picker,
   //triggering warnings in editor but not in app.
@@ -87,25 +87,25 @@ const _Tracking = (props) => {
   })*/
 
   /******REALM WORK LOG LOGIC************************* */
-  const [logsExist, setLogsExist] = useState (realm.objects('WorkLog').length>0)
+  const [logsExist, setLogsExist] = useState(realm.objects('WorkLog').length > 0)
   const [logsFromDB, setLogsFromDB] = useState([]);
 
-  const realmWorkLogListener = ()=>{
+  const realmWorkLogListener = () => {
     setLogsFromDB(realm.objects('WorkLog'));
-    setLogsExist(realm.objects('WorkLog').length>0)
+    setLogsExist(realm.objects('WorkLog').length > 0)
   }
-  realm.addListener("change",realmWorkLogListener)
-  useEffect(()=>{
+  realm.addListener("change", realmWorkLogListener)
+  useEffect(() => {
     //clean up function
-    return ()=>{
-      realm.removeListener("change",realmWorkLogListener)
+    return () => {
+      realm.removeListener("change", realmWorkLogListener)
     }
-  },[])
+  }, [])
 
 
-  useEffect(()=>{
+  useEffect(() => {
     //set it to run only if the logs collection from mongodb is not empty
-    if(logsExist){
+    if (logsExist) {
       setLogsFromDB(realm.objects('WorkLog'));
       //somehow jobsFromDB after this line has not data
       /*setItems(jobsFromDB.map((e)=>{
@@ -113,12 +113,12 @@ const _Tracking = (props) => {
       }))
       */
     }
-  },[logsExist])
+  }, [logsExist])
 
 
-  const clearLogs = ()=>{
+  const clearLogs = () => {
 
-    realm.write(()=>{
+    realm.write(() => {
       let allLogs = realm.objects('WorkLog');
       realm.delete(allLogs);
       console.log('all logs deleted')
@@ -130,21 +130,21 @@ const _Tracking = (props) => {
 
   /******REALM JOB LOGIC ***************************** */
 
-  const realmListener = ()=>{
+  const realmListener = () => {
     setJobsFromDB(realm.objects('Job'))
     //update jobsExists
-    setJobsExist(realm.objects('Job').length>0)
+    setJobsExist(realm.objects('Job').length > 0)
   }
-  realm.addListener("change",realmListener)
-  useEffect(()=>{
+  realm.addListener("change", realmListener)
+  useEffect(() => {
     //clean up function
-    return ()=>{
-      realm.removeListener("change",realmListener)
+    return () => {
+      realm.removeListener("change", realmListener)
     }
-  },[])
-  
+  }, [])
 
-  const [jobsExist, setJobsExist] = useState (realm.objects('Job').length>0)
+
+  const [jobsExist, setJobsExist] = useState(realm.objects('Job').length > 0)
   const [jobsFromDB, setJobsFromDB] = useState([]);
 
   //debug check:
@@ -152,9 +152,9 @@ const _Tracking = (props) => {
   const [localJobId, setLocalJobId] = useState(jobId)
 
   //used to populate the item array for the dropdown picker
-  useEffect(()=>{
+  useEffect(() => {
     //set it to run only if the jobs collection from mongodb is not empty
-    if(jobsExist){
+    if (jobsExist) {
       setJobsFromDB(realm.objects('Job'));
       //somehow jobsFromDB after this line has not data
       /*setItems(jobsFromDB.map((e)=>{
@@ -162,36 +162,36 @@ const _Tracking = (props) => {
       }))
       */
     }
-  },[jobsExist])
+  }, [jobsExist])
 
   //second function to update items in picker if jobsFromDB gets updated
-  useEffect(()=>{
-    if(jobsFromDB.length!==0){
-      setItems(jobsFromDB.map((e)=>{
-        return {label:e.employer,value:e.id}
+  useEffect(() => {
+    if (jobsFromDB.length !== 0) {
+      setItems(jobsFromDB.map((e) => {
+        return { label: e.employer, value: e.id }
       }))
-    }else{
+    } else {
       //empty the items
       setItems([])
       //set the jobID back to default
     }
-  },[jobsFromDB])
+  }, [jobsFromDB])
 
   //debug function to compare the current selected job id with the job id at the global state
   //edit* not acutally a debug functiion, necessary to call setJobId indirectly outside the picker
-  useEffect(()=>{
+  useEffect(() => {
     setJobId(localJobId)
-  },[localJobId])
+  }, [localJobId])
 
 
 
   //sample jobs for testing purposes, will be discarded
-  const generateIDInt = ()=>{
-    return Math.floor(Math.random()*100000);
+  const generateIDInt = () => {
+    return Math.floor(Math.random() * 100000);
   }
 
   //no longer async, had to remove them to get rid of memory leaks
-  const addJobs = () =>{
+  const addJobs = () => {
     let object = {
       id: generateIDInt(),
       employer: 'Kyle',
@@ -199,14 +199,14 @@ const _Tracking = (props) => {
       location: 'test location',
       color: 'rgba(245, 40, 145, 0.35)'
     }
-    realm.write(()=>{
-      realm.create('Job',object)
+    realm.write(() => {
+      realm.create('Job', object)
     })
   }
-  const clearJobs = ()=>{
+  const clearJobs = () => {
     setLocalJobId(-5)
 
-    realm.write(()=>{
+    realm.write(() => {
       let allJobs = realm.objects('Job');
       realm.delete(allJobs);
       console.log('all jobs deleted')
@@ -216,102 +216,102 @@ const _Tracking = (props) => {
 
   const [showWorkLogs, setShowWorkLogs] = useState(false)
   const [showWorkLogsText, setShowWorkLogsText] = useState('show work logs')
-  const showWorkLogButton = ()=>{
-    if (showWorkLogs){
+  const showWorkLogButton = () => {
+    if (showWorkLogs) {
       setShowWorkLogsText('show work logs')
       setShowWorkLogs(false)
     }
-    else{
+    else {
       setShowWorkLogsText('hide work logs')
       setShowWorkLogs(true)
     }
   }
 
 
- 
+
 
 
   return (
 
     <View style={styles.container}>
       {/* <Text style={[styles.elements, global.globalCustomFontUse ? { fontFamily: 'SFPro-Regular' } : {}]}>Job: Default Job</Text> */}
-     
-     {showTimer
-      ?<Timer /> 
-      :<View></View>
-     }
-  
-    {
-      debugInfo
-      ?
-        <View>
-          <Text>localJobId: {JSON.stringify(localJobId)}</Text>
-          <Text>jobId (redux global state): {JSON.stringify(jobId)}</Text>
-          <Text>jobs stored in the picker: {JSON.stringify(items)}</Text>
 
-        </View>
-      : <View></View>
-    }
+      {showTimer
+        ? <Timer />
+        : <View></View>
+      }
+
+      {
+        debugInfo
+          ?
+          <View>
+            <Text>localJobId: {JSON.stringify(localJobId)}</Text>
+            <Text>jobId (redux global state): {JSON.stringify(jobId)}</Text>
+            <Text>jobs stored in the picker: {JSON.stringify(items)}</Text>
+
+          </View>
+          : <View></View>
+      }
 
 
-    <TouchableOpacity onPress={showWorkLogButton}>
-      <Text>{showWorkLogsText}</Text>
-    </TouchableOpacity>
-    {
-      showWorkLogs
-      ?<View>
-        <Text>worklogs: {JSON.stringify(logsFromDB)}</Text>
-      </View>
-      :<View></View>
-    }
+      <TouchableOpacity onPress={showWorkLogButton}>
+        <Text>{showWorkLogsText}</Text>
+      </TouchableOpacity>
+      {
+        showWorkLogs
+          ? <View>
+            <Text>worklogs: {JSON.stringify(logsFromDB)}</Text>
+          </View>
+          : <View></View>
+      }
 
 
 
 
       {
         jobsExist
-        ?<View>
-          {
-            isTracking
-            ?<Text>Currently tracking a selected job, stop location tracking first to switch jobs</Text>
-            :<DropDownPicker
-              style={styles.picker}
-              placeholder="Select a Job"
-              containerStyle={styles.pickerContainer}
-              placeholderStyle={styles.pickerLabel}
-              labelStyle={styles.pickerLabel}
-              itemSeparator={true}
-              open={open}
-              value={value}
-              items={items}
-              setOpen={setOpen}
-              //Still seems to work properly despite undeclared setValue property warning
-              setValue={setValue}
-              setItems={setItems}
-    
-              onSelectItem={(item) => {
-                //This is where we'll record the job selection and pass to 'ActiveTracking' DB
-    
-                setLocalJobId(item.value);
-                console.log(item);
-              }}
-            //displaying the jobs:
-            />
-          }
+          ? <View style={styles.container}>
+            {
+              isTracking
+                ? <Text>Currently tracking a selected job, stop location tracking first to switch jobs</Text>
+                : <DropDownPicker
+                  style={styles.picker}
+                  placeholder="Select a Job"
+                  containerStyle={styles.pickerContainer}
+                  placeholderStyle={styles.pickerLabel}
+                  labelStyle={styles.pickerLabel}
+                  itemSeparator={true}
+                  open={open}
+                  value={value}
+                  items={items}
+                  setOpen={setOpen}
+                  //Still seems to work properly despite undeclared setValue property warning
+                  setValue={setValue}
+                  setItems={setItems}
 
-          <LocationMap/>
-        </View>
-        :<View>
-          <Text>No jobs to track, add jobs from the setup screen to begin tracking</Text>
-          <TouchableOpacity onPress={()=>{addJobs()}}>
-            <Text>Or click me to add some jobs</Text>
-          </TouchableOpacity>
-        </View>
+                  onSelectItem={(item) => {
+                    //This is where we'll record the job selection and pass to 'ActiveTracking' DB
+
+                    setLocalJobId(item.value);
+                    console.log(item);
+                  }}
+                //displaying the jobs:
+                />
+            }
+
+            <LocationMap />
+          </View>
+          : <View>
+            <Text>No jobs to track, add jobs from the setup screen to begin tracking</Text>
+            <TouchableOpacity onPress={() => { addJobs() }}>
+              <Text>Or click me to add some jobs</Text>
+            </TouchableOpacity>
+          </View>
       }
 
     </View>
   );
 }
 
-const Tracking= connect(mapStateToProps,mapDispatchToProps)(_Tracking);
+const Tracking = connect(mapStateToProps, mapDispatchToProps)(_Tracking);
 export default Tracking;
